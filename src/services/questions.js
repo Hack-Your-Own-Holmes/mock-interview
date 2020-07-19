7a3402e98e2b70de3e33cb4511d3b9801d6185eb
Connor Adams <cnnr252@gmail.com>
2020-07-19 19:01:33 +0000

    Lint running on github workflows

--- src/services/questions.js
+++ src/services/questions.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const URL = `http://localhost:${process.env.PORT}/questions`;
 
 export const getQuestions = async () => {
-	const { data } = await axios.get(URL);
-	return data;
+    const {data} = await axios.get(URL);
+    return data;
 };
