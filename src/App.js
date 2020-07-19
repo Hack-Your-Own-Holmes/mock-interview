3d722668603c9c90611223165acd9edd748aac40
Shabnam Nikpour <snikpou2@uwo.ca>
2020-07-19 17:53:32 -0400

    The webcam service added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./styles/main.css";
-import {getQuestions} from "./services/questions";
+import { getQuestions } from "./services/questions";
+import videoStream from "./services/videoStream";
+import WebcamStreamCapture from "./services/videoStream";
 
 function App() {
-    const handleGet = () => {
-        getQuestions().then((questions) => {
-            console.log(questions);
-        });
-    };
-    return (
-        <div>
-            <button onClick={handleGet}>Get questions</button>
-        </div>
-    );
+	const handleGet = () => {
+		getQuestions().then((questions) => {
+			console.log(questions);
+		});
+	};
+	return (
+		<div>
+			<button onClick={handleGet}>Get questions</button>
+			<WebcamStreamCapture />
+		</div>
+	);
 }
 
 export default App;
