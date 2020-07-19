4009b1fd28612805dc581af53a7b58a8f0e6f5cc
Connor Adams <cnnr252@gmail.com>
2020-07-19 20:14:16 +0000

    Fix docker build and add docker build test to github workflow

--- src/services/questions.js
+++ src/services/questions.js
@@ -1,8 +1,4 @@
-import axios from "axios";
-
-const URL = `http://localhost:${process.env.PORT}/questions`;
-
 export const getQuestions = async () => {
-    const {data} = await axios.get(URL);
-    return data;
+	const { data } = await fetch("/questions");
+	return data;
 };
